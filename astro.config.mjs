8a3ad8dd0f56a2b57e7f316a4c8539ba86d18dbe
brkunver <burakhanunver@gmail.com>
2025-03-25 16:20:02 +0300

    remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://astro.build/config
 export default defineConfig({
   site: "https://brkunver.github.io",
-  base: "/llm-toolbox-extension",
+  //base: "/llm-toolbox-extension",
   vite: {
     plugins: [tailwindcss()],
   },
