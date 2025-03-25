38bc50075395376c9341cef7532bb3d872f7d597
brkunver <burakhanunver@gmail.com>
2025-03-25 16:18:01 +0300

    add site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite"
 
 // https://astro.build/config
 export default defineConfig({
-  site : "https",
+  site: "https://brkunver.github.io",
+  base: "/llm-toolbox-extension",
   vite: {
-    plugins: [tailwindcss()]
-  }
-});
+    plugins: [tailwindcss()],
+  },
+})
