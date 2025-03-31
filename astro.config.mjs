8ca9b964ad5509124a1624f255b382ebec450d64
brkunver <burakhanunver@gmail.com>
2025-03-31 16:33:40 +0300

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config"
-
 import tailwindcss from "@tailwindcss/vite"
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://brkunver.github.io",
-  //base: "/llm-toolbox-extension",
+  site: "https://brkunver.github.io/llm-toolbox-extension",
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 })
